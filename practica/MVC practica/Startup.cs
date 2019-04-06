641459417f11855058bf972ca3b8e7596a9b30be
Andres Moron <andresmoron622@gmail.com>
2019-04-06 00:00:28 -0400

    Commit MVC

---
+++ practica/MVC practica/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_practica.Startup))]
+namespace MVC_practica
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
